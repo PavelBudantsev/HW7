caf55227bee7b7204f88b0ad2755ee1610cd0d93
PavelBudantsev <p.budantsev@ya.ru>
2022-08-21 16:17:42 +0500

    add 4th tasks

---
+++ HW4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// Console.WriteLine("Введите основание степени ");
+// int basis = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите показатель степени ");
+// int indicator = Convert.ToInt32(Console.ReadLine());
+// int res = 1;
+
+// for (int i = 1; i <= indicator; i++)
+// {
+//     res = res * basis;
+// }
+
+// Console.WriteLine(res);
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// Console.WriteLine("Введите число ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int res = 0;
+// while (numb>0)
+// {
+//     res = res+numb%10;
+//     numb = numb/10;
+// }
+// Console.WriteLine(res);
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// int dim = 8; // задаем размерность массива, потом проще будет поменять, если размерность изменится
+// int[] Arr = new int[dim];
+// Console.WriteLine("Введите восемь чисел через enter");
+// for (int i = 0; i < dim; i++)  
+//    Arr[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"[{String.Join(", ",Arr)}]");
