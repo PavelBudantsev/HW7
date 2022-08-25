774515e524bb5ef171b879a0b1ca4199219dc928
PavelBudantsev <p.budantsev@ya.ru>
2022-08-25 19:08:35 +0500

    git init

---
+++ HW5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// int[] array = new int[12];
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Random rand = new Random();
+//     array[i] = rand.Next(100, 1000);
+//     if (array[i] % 2 == 0) count++;
+// }
+// Console.WriteLine(count);
+
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// int[] array = new int[12];
+// int sum = 0;
+
+// for (int i = 1; i < array.Length; i = i + 2)
+// {
+//     Random r = new Random();
+//     array[i] = r.Next();
+//     sum = sum + array[i];
+// }
+// Console.WriteLine(sum);
+
+// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+Double[] array = new Double[12];
+Double min = 0;
+Double max = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Random rand = new Random();
+    array[i] = rand.NextDouble();
+    if(array[i] < min) min = array[i];  
+    if(array[i] > max) max = array[i];
+}
+Console.WriteLine(max-min);
+Console.WriteLine(String.Join(" ", array));
